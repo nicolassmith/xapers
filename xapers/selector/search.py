d4f96e5a8d59e5ada5de09eed6cc61dc04d31de3
Jameson Graef Rollins <jrollins@finestructure.net>
2012-09-12 15:15:40 +0200

    selector: break out search into separate class file, and add doc field editing

---
+++ xapers/selector/search.py
@@ -0,0 +1,229 @@
+import subprocess
+import urwid
+
+from xapers.database import Database
+from xapers.documents import Document
+
+class DocListItem(urwid.WidgetWrap):
+    def __init__(self, doc, percent):
+        self.doc = doc
+        self.percent = percent
+        self.docid = self.doc.get_docid()
+        self.path = self.doc.get_fullpaths()[0]
+        self.sources = self.doc.get_sources()
+        self.tags = urwid.Text(' '.join(self.doc.get_tags()))
+        self.url = urwid.Text(self.doc.get_url())
+        self.title = urwid.Text(self.doc.get_title())
+        self.authors = urwid.Text(self.doc.get_authors())
+        self.year = urwid.Text(self.doc.get_year())
+        self.data = urwid.Text(self.doc.get_data())
+
+        #self.tag_string = '(%s)' % (' '.join(self.tags))
+        self.source_string = '['
+        for source,sid in self.sources.items():
+            self.source_string += '%s:%s' % (source, sid)
+        self.source_string += ']'
+
+        self.c1width = 10
+
+        self.rowHeader = urwid.Columns(
+            [('fixed', self.c1width,
+              urwid.AttrWrap(
+                  urwid.Text('id:%s' % (self.docid)),
+                  'head_id',
+                  'focus_id')),
+             ('fixed', 4,
+              urwid.AttrWrap(
+                  urwid.Text('%i%%' % (self.percent)),
+                  'head_percent',
+                  'focus')),
+             ('fixed', len(self.path),
+              urwid.AttrWrap(
+                  urwid.Text('%s' % (self.path)),
+                  'head_path',
+                  'focus')),
+             # ('fixed', len(self.tag_string),
+             #  urwid.AttrWrap(
+             #      urwid.Text('%s' % (self.tag_string)),
+             #      'head_tags',
+             #      'focus')),
+             ('fixed', len(self.source_string),
+              urwid.AttrWrap(
+                  urwid.Text('%s' % (self.source_string)),
+                  'head_sources',
+                  'focus')),
+             ],
+            dividechars=1,
+            )
+
+        w = urwid.Pile(
+            [
+                urwid.Divider('-'),
+                self.rowHeader,
+                self.docfield('tags'),
+                self.docfield('url'),
+                self.docfield('title'),
+                self.docfield('authors'),
+                self.docfield('year'),
+                self.docfield('data'),
+                ]
+            ,
+            focus_item=1)
+        self.__super.__init__(w)
+
+    def docfield(self, field):
+        return urwid.Columns(
+            [
+                ('fixed', self.c1width,
+                 urwid.AttrWrap(
+                        urwid.Text(field + ':'),
+                        'data_bold', 'focus')),
+                eval('self.' + field),
+            # ('fixed', self.c1width,
+            #   urwid.AttrWrap(
+            #             urwid.Text(field + ':'),
+            #             'data_bold',
+            #             'focus')),
+            #  urwid.AttrWrap(
+            #         urwid.Text('%s' % (value)),
+            #         'body',
+            #         'focus')
+                ]
+            )
+
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        return key
+
+class CustomEdit(urwid.Edit):
+    __metaclass__ = urwid.signals.MetaSignals
+    signals = ['done']
+
+    def keypress(self, size, key):
+        if key == 'enter':
+            urwid.emit_signal(self, 'done', self.get_edit_text())
+            return
+        elif key == 'esc':
+            urwid.emit_signal(self, 'done', None)
+            return
+
+        urwid.Edit.keypress(self, size, key)
+
+class Search(urwid.WidgetWrap):
+    def __init__(self, ui, query):
+        self.ui = ui
+        self.db = Database(self.ui.xdir, writable=False)
+
+        matches = self.db.search(query, limit=20)
+
+        items = []
+        for m in matches:
+            doc = Document(self.db, doc=m.document)
+            items.append(DocListItem(doc, m.percent))
+
+        self.listwalker = urwid.SimpleListWalker(items)
+        self.listbox = urwid.ListBox(self.listwalker)
+        #w = urwid.Frame(urwid.AttrWrap(self.listbox, 'body'))
+        #w = urwid.AttrWrap(self.listbox, 'body')
+        w = self.listbox
+        self.__super.__init__(w)
+
+    def nextEntry(self):
+        # listbox.set_focus(listbox.get_next())
+        pos = self.listbox.get_focus()[1]
+        self.listbox.set_focus(pos + 1)
+        # self.listbox.keypress(1, 'down')
+
+    def prevEntry(self):
+        pos = self.listbox.get_focus()[1]
+        if pos == 0: return
+        self.listbox.set_focus(pos - 1)
+
+    def viewEntry(self):
+        docid = self.listbox.get_focus()[0].docid
+        path = self.listbox.get_focus()[0].path
+        path = path.replace(' ','\ ')
+        message = 'opening doc id:%s...' % docid
+        self.ui.set_status(message)
+        subprocess.call(' '.join(["nohup", "okular", path]) + ' &',
+                        shell=True,
+                        stdout=open('/dev/null','w'),
+                        stderr=open('/dev/null','w'))
+
+    def tag(self, sign):
+        # focus = self.listbox.get_focus()[0]
+        # tags = focus.tags
+        if sign is '+':
+            prompt = 'add tag: '
+        elif sign is '-':
+            prompt = 'remove tag: '
+        self.foot = CustomEdit(prompt)
+        self.ui.view.set_footer(self.foot)
+        self.ui.view.set_focus('footer')
+        urwid.connect_signal(self.foot, 'done', self.tag_done, sign)
+
+    def tag_done(self, tag, sign):
+        self.ui.view.set_focus('body')
+        urwid.disconnect_signal(self, self.foot, 'done', self.tag_done)
+        focus = self.listbox.get_focus()[0]
+        docid = focus.docid
+        db = Database(self.ui.xdir, writable=True)
+        doc = db.get_doc(docid)
+        if sign is '+':
+            msg = "Added tag '%s'" % (tag)
+            doc.add_tags(content)
+        elif sign is '-':
+            msg = "Removed tag '%s'" % (tag)
+            doc.remove_tags(tag)
+        tags = doc.get_tags()
+        focus.tags.set_text(' '.join(tags))
+        self.ui.set_status(msg)
+
+    def setField(self, field):
+        focus = self.listbox.get_focus()[0]
+        element = eval('focus.' + field)
+        value = element.get_text()[0]
+        self.foot = CustomEdit(field + ': ', edit_text=value)
+        self.ui.view.set_footer(self.foot)
+        self.ui.view.set_focus('footer')
+        urwid.connect_signal(self.foot, 'done', self.setField_done, field)
+
+    def setField_done(self, new, field):
+        self.ui.view.set_focus('body')
+        urwid.disconnect_signal(self, self.foot, 'done', self.setField_done)
+        if new is not None:
+            focus = self.listbox.get_focus()[0]
+            docid = focus.docid
+            db = Database(self.ui.xdir, writable=True)
+            doc = db.get_doc(docid)
+            eval('doc.set_' + field + '("' + new + '")')
+            element = eval('focus.' + field)
+            element.set_text(new)
+            msg = "Document id:%s %s updated." % (focus.docid, field)
+        else:
+            msg = "Nothing done."
+        self.ui.set_status(msg)
+
+    def keypress(self, size, key):
+        if key is 'n':
+            self.nextEntry()
+        elif key is 'p':
+            self.prevEntry()
+        elif key is '+':
+            self.tag('+')
+        elif key is '-':
+            self.tag('-')
+        elif key is 'U':
+            self.setField('url')
+        elif key is 'T':
+            self.setField('title')
+        elif key is 'A':
+            self.setField('authors')
+        elif key is 'Y':
+            self.setField('year')
+        elif key is 'enter':
+            self.viewEntry()
+        else:
+            self.ui.keypress(key)
--- xapers/selector/ui.py
+++ xapers/selector/ui.py
@@ -1,160 +1,9 @@
+import sys
 import subprocess
 import urwid
 
 from xapers.database import Database
 from xapers.documents import Document
-import xapers.nci as nci
-
-class Search(urwid.WidgetWrap):
-    def __init__(self, ui, args):
-        self.ui = ui
-        self.db = Database(self.ui.xdir, writable=False)
-
-        matches = self.db.search(args, limit=20)
-
-        items = []
-        for m in matches:
-            doc = Document(self.db, doc=m.document)
-            items.append(DocListItem(doc, m.percent))
-
-        self.listwalker = urwid.SimpleListWalker(items)
-        self.listbox = urwid.ListBox(self.listwalker)
-        #w = urwid.Frame(urwid.AttrWrap(self.listbox, 'body'))
-        w = urwid.AttrWrap(self.listbox, 'body')
-        self.__super.__init__(w)
-
-    def nextEntry(self):
-        # listbox.set_focus(listbox.get_next())
-        pos = self.listbox.get_focus()[1]
-        self.listbox.set_focus(pos + 1)
-        # self.listbox.keypress(1, 'down')
-
-    def prevEntry(self):
-        pos = self.listbox.get_focus()[1]
-        if pos == 0: return
-        self.listbox.set_focus(pos - 1)
-
-    def viewEntry(self):
-        docid = self.listbox.get_focus()[0].docid
-        path = self.listbox.get_focus()[0].path
-        path = path.replace(' ','\ ')
-        message = 'opening doc id:%s...' % docid
-        self.ui.set_status(message)
-        subprocess.call(' '.join(["nohup", "okular", path]) + ' &',
-                        shell=True,
-                        stdout=open('/dev/null','w'),
-                        stderr=open('/dev/null','w'))
-
-    def editEntry(self):
-        return
-        doc = self.listbox.get_focus()[0].doc
-
-        self.prompt(prefix='add tag: ')
-
-        wdb = Database(self.xdir, writable=True)
-        wdoc = Document(wdb, doc=doc.doc)
-        wdoc.add_tags(['QUX'])
-
-    def keypress(self, size, key):
-        if key is 'n':
-            self.nextEntry()
-        elif key is 'p':
-            self.prevEntry()
-        elif key is 'e':
-            self.editEntry()
-        elif key is 'enter':
-            self.viewEntry()
-        else:
-            self.ui.keypress(key)
-
-
-class DocListItem(urwid.WidgetWrap):
-    def __init__(self, doc, percent):
-        self.doc = doc
-        self.percent = percent
-        self.docid = self.doc.get_docid()
-        self.path = self.doc.get_fullpaths()[0]
-        self.tags = self.doc.get_tags()
-        self.sources = self.doc.get_sources()
-        self.url = self.doc.get_url()
-        self.title = self.doc.get_title()
-        self.authors = self.doc.get_authors()
-        if not self.authors: self.authors = ''
-        self.year = self.doc.get_year()
-        self.data = self.doc.get_data()
-
-        self.tag_string = '(%s)' % (' '.join(self.tags))
-        self.source_string = '['
-        for source,sid in self.sources.items():
-            self.source_string += '%s:%s' % (source, sid)
-        self.source_string += ']'
-
-        self.c1width = 10
-
-        self.rowHeader = urwid.Columns(
-            [('fixed', self.c1width,
-              urwid.AttrWrap(
-                  urwid.Text('id:%s' % (self.docid)),
-                  'head_id',
-                  'focus_id')),
-             ('fixed', 4,
-              urwid.AttrWrap(
-                  urwid.Text('%i%%' % (self.percent)),
-                  'head_percent',
-                  'focus')),
-             ('fixed', len(self.path),
-              urwid.AttrWrap(
-                  urwid.Text('%s' % (self.path)),
-                  'head_path',
-                  'focus')),
-             ('fixed', len(self.tag_string),
-              urwid.AttrWrap(
-                  urwid.Text('%s' % (self.tag_string)),
-                  'head_tags',
-                  'focus')),
-             ('fixed', len(self.source_string),
-              urwid.AttrWrap(
-                  urwid.Text('%s' % (self.source_string)),
-                  'head_sources',
-                  'focus')),
-             ],
-            dividechars=1,
-            )
-
-        w = urwid.Pile(
-            [
-                urwid.Divider('-'),
-                self.rowHeader,
-                self.docfield('url', self.url),
-                self.docfield('title', self.title),
-                self.docfield('author', self.authors),
-                self.docfield('year', self.year),
-                self.docfield('data', self.data),
-                ]
-            ,
-            focus_item=1)
-        self.__super.__init__(w)
-
-    def docfield(self, field, value):
-        return urwid.Columns(
-            [('fixed', self.c1width,
-              urwid.AttrWrap(
-                        urwid.Text(field + ':'),
-                        'data_bold',
-                        'focus')),
-             urwid.AttrWrap(
-                    urwid.Text('%s' % (value)),
-                    'body',
-                    'focus')
-             ]
-            )
-
-    def selectable (self):
-        return True
-
-    def keypress(self, size, key):
-        return key
-
 
 class UI():
 
@@ -168,20 +17,31 @@ class UI():
         ('title', 'dark green', ''),
         ('body', 'dark cyan', ''),
         ('focus', 'black', 'dark cyan'),
+
+        ('header', 'white', 'dark blue'),
         ('footer', 'white', 'dark blue'),
         ]
 
     def __init__(self, xdir, cmd, args):
         self.xdir = xdir
+        self.view = urwid.Frame(urwid.SolidFill())
 
         if cmd is 'search':
+            from .search import Search
             self.cmd = Search(self, args)
+            self.title = "search: " + args
+        elif cmd is 'edit':
+            from .edit import Edit
+            self.cmd = Edit(self, args)
+            self.title = "edit: " + args
         else:
             print >>sys.stderr, "Unknown command:", cmd
             sys.exit()
 
-        self.view = urwid.Frame(self.cmd)
-        self.set_status()
+        self.view = urwid.Frame(urwid.AttrWrap(self.cmd, 'body'))
+
+        self.set_header(self.title)
+        self.set_status('foo')
         self.mainloop = urwid.MainLoop(
             self.view,
             self.palette,
@@ -190,11 +50,18 @@ class UI():
             )
         self.mainloop.run()
 
+    def set_header(self, text=None):
+        if not text:
+            message = 'Xapers'
+        else:
+            message = 'Xapers %s' % (text)
+        self.view.set_header(urwid.AttrWrap(urwid.Text(message), 'header'))
+
     def set_status(self, text=None):
         if not text:
             message = 'Xapers'
         else:
-            message = 'Xapers: %s' % (text)
+            message = '%s' % (text)
         self.view.set_footer(urwid.AttrWrap(urwid.Text(message), 'footer'))
 
     def keypress(self, key):
