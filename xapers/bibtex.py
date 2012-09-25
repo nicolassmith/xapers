b53e101b783d5aa18bd5585c6ae306c1b2380d8d
Jameson Graef Rollins <jrollins@finestructure.net>
2012-09-24 21:37:56 -0700

    bibtex: fix bibtex parser
    use regular io, instead of cStringIO

--- xapers/bibtex.py
+++ xapers/bibtex.py
@@ -1,5 +1,6 @@
 import sys
 import cStringIO
+import io
 from pybtex.database.input import bibtex
 
 def bibtrans(string):
@@ -8,37 +9,41 @@ def bibtrans(string):
     return string
 
 def parse(bib):
-    bibfile = cStringIO.StringIO()
-    bibfile.write(bib)
-    parser = bibtex.Parser()
-    #bibdata = parser.parse_file('test/sources/doi.bib')
-    #bibdata = parser.parse_file('/home/jrollins/ligo/src/papers/multiColorMetrology/GWreferences.bib')
-    try:
-        bibdata = parser.parse_stream(bib)
-    except:
-        print >>sys.stderr, "Unable to parse bibtex."
-        return None
+    # bibfile = cStringIO.StringIO()
+    # bibfile.write(bib)
+    bibfile = io.StringIO(unicode(bib))
 
-    bibentry = bibdata.entries.values()[0].fields
+    parser = bibtex.Parser(encoding='UTF-8')
+    bibdata = parser.parse_stream(bibfile)
+
     # for key in bibdata.entries.keys():
     #     print key
     #     for field, val in bibdata.entries[key].fields.iteritems():
     #         print '   ', field, '=', val
+    #     print '   ', 'authors =', ' and '.join(bibdata.entries[key].persons['author'])
+
+    bibentry = bibdata.entries.values()[0].fields
+
+    authors = []
+    for p in bibdata.entries.values()[0].persons['author']:
+        authors.append('%s %s' % (p.first()[0], p.last()[0]))
 
     data = {
+        'url': None,
         'title': None,
         'authors': None,
         'year': None,
         'abstract': None
         }
 
+    if 'url' in bibentry:
+        data['url'] = bibtrans(bibentry['url']).encode('utf-8')
     if 'title' in bibentry:
-        data['title'] = bibtrans(bibentry['title'])
-    if 'authors' in bibentry:
-        data['authors'] = bibtrans(bibentry['author'])
+        data['title'] = bibtrans(bibentry['title']).encode('utf-8')
+    data['authors'] = authors
     if 'year' in bibentry:
-        data['year'] = bibentry['year']
+        data['year'] = bibentry['year'].encode('utf-8')
     if 'abstract' in bibentry:
-        data['abstract'] = bibtrans(bibentry['abstract'])
+        data['abstract'] = bibtrans(bibentry['abstract']).encode('utf-8')
 
     return data
