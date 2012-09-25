abccdeef1d96f4f2923aab71314b22cb5d7c678c
Jameson Graef Rollins <jrollins@finestructure.net>
2012-09-25 06:55:50 -0700

    sources: fix sid initialization in doi source

--- xapers/sources/doi.py
+++ xapers/sources/doi.py
@@ -2,19 +2,20 @@ class Source():
     name = 'doi'
     urlbase = 'dx.doi.org'
 
+    def __init__(self, sid=None):
+        self.sid = sid
+
     def parse_url(self, parsedurl):
         sid = None
         loc = parsedurl.netloc
         path = parsedurl.path
-        if loc.find(self.urlbase) < 0:
-            self.sid = None
-        else:
+        if loc.find(self.urlbase) >= 0:
             self.sid = path.strip('/')
 
     def get_bibtex(self):
         import urllib2
 
-        if self.file:
+        if 'file' in dir(self):
             f = open(self.file, 'r')
         else:
             url = 'http://' + self.urlbase + '/' + self.sid
