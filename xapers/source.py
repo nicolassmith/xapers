9ec2d0ab2ad77d4093ead255974da90d7d14e661
Jameson Graef Rollins <jrollins@finestructure.net>
2012-09-25 06:39:44 -0700

    sources: rework sources into actual classes
    This makes it much easier to carry data around
    
    Also tweak to allow parsing of files instead of urls

--- xapers/source.py
+++ xapers/source.py
@@ -1,24 +1,46 @@
+import os
 import sys
+from urlparse import urlparse
+
+import xapers.sources
+
+def list_sources():
+    sources = []
+    return ['doi']
+    for s in dir(xapers.sources):
+        # skip the __init__ file when finding sources
+        if '__' in s:
+            continue
+        sources.append(s)
+    return sources
 
 def source_from_url(url):
     source = None
     sid = None
 
-    from urlparse import urlparse
-    o = urlparse(url)
+    if os.path.exists(url):
+        name = os.path.basename(url)
+
     # ParseResult(scheme='http', netloc='www.cwi.nl:80', path='/%7Eguido/Python.html',
     #             params='', query='', fragment='')
     
-    import xapers.sources
-
-    for ss in dir(xapers.sources):
-        if '__' in ss:
-            continue
+    for ss in list_sources():
         print >>sys.stderr, 'trying', ss, '...', 
-        exec('import xapers.sources.' + ss + ' as smod')
-        sid = smod.parse_url(o)
-        if sid:
-            source = ss
+        #exec('import xapers.sources.' + ss + ' as smod')
+        exec('from xapers.sources.' + ss + ' import Source')
+
+        smod = Source()
+
+        if os.path.exists(url):
+            base, ext = os.path.splitext(os.path.basename(url))
+            if base == ss:
+                smod.sid = '1234'
+                smod.file = url
+        else:
+            o = urlparse(url)
+            smod.parse_url(o)
+
+        if smod.sid:
             print >>sys.stderr, 'match!'
             break
         else:
@@ -28,7 +50,7 @@ def source_from_url(url):
     if not smod:
         print >>sys.stderr, 'no matching source module found.'
 
-    return smod, sid
+    return smod
 
 
 def get_source(source):
--- xapers/sources/doi.py
+++ xapers/sources/doi.py
@@ -1,38 +1,27 @@
-name = 'doi'
-urlbase = 'dx.doi.org'
+class Source():
+    name = 'doi'
+    urlbase = 'dx.doi.org'
 
-def parse_url(parsedurl):
-    sid = None
-    loc = parsedurl.netloc
-    path = parsedurl.path
-    if loc.find(urlbase) < 0:
-        return None
-    sid = path.strip('/')
-    return sid
+    def parse_url(self, parsedurl):
+        sid = None
+        loc = parsedurl.netloc
+        path = parsedurl.path
+        if loc.find(self.urlbase) < 0:
+            self.sid = None
+        else:
+            self.sid = path.strip('/')
 
-def get_data(sid, lfile=None):
-    import urllib2
+    def get_bibtex(self):
+        import urllib2
 
-    if lfile:
-        f = open(lfile, 'r')
-    else:
-        url = 'http://' + urlbase + '/' + sid
-        headers = dict(Accept='text/bibliography; style=bibtex')
-        req = urllib2.Request(url, headers=headers)
-        f = urllib2.urlopen(req)
-    bibtex = f.read()
-    f.close
+        if self.file:
+            f = open(self.file, 'r')
+        else:
+            url = 'http://' + self.urlbase + '/' + self.sid
+            headers = dict(Accept='text/bibliography; style=bibtex')
+            req = urllib2.Request(url, headers=headers)
+            f = urllib2.urlopen(req)
+        bibtex = f.read()
+        f.close
 
-    import xapers.bibtex
-    bdata = xapers.bibtex.parse(bibtex)
-
-    data = {
-        'source':  'doi',
-        'sid':     sid,
-        'title':   bdata['title'],
-        'authors': bdata['authors'],
-        'year':    bdata['year'],
-        'bibtex':  bibtex,
-        }
-
-    return data
+        return bibtex
