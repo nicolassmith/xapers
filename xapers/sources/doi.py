83db83c0c61804eb75a7230091fb63733da01ec2
Jameson Graef Rollins <jrollins@finestructure.net>
2012-09-27 12:17:54 -0700

    sources: cleanup up doi module

--- xapers/sources/doi.py
+++ xapers/sources/doi.py
@@ -1,24 +1,26 @@
+import urllib2
+
 class Source():
-    name = 'doi'
-    urlbase = 'dx.doi.org'
+    source = 'doi'
+    netloc = 'dx.doi.org'
 
     def __init__(self, sid=None):
         self.sid = sid
 
+    def gen_url(self):
+        return 'http://%s/%s' % (self.netloc, self.sid)
+
     def parse_url(self, parsedurl):
-        sid = None
         loc = parsedurl.netloc
         path = parsedurl.path
-        if loc.find(self.urlbase) >= 0:
+        if loc.find(self.netloc) >= 0:
             self.sid = path.strip('/')
 
     def get_bibtex(self):
-        import urllib2
-
         if 'file' in dir(self):
             f = open(self.file, 'r')
         else:
-            url = 'http://' + self.urlbase + '/' + self.sid
+            url = self.gen_url()
             headers = dict(Accept='text/bibliography; style=bibtex')
             req = urllib2.Request(url, headers=headers)
             f = urllib2.urlopen(req)
