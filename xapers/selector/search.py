cec992ba3a228919130ed0bcb3133dc7c78195fa
Jameson Graef Rollins <jrollins@finestructure.net>
2012-09-29 00:10:52 -0700

    selector: update for new lib changes

--- xapers/selector/search.py
+++ xapers/selector/search.py
@@ -7,55 +7,67 @@ from xapers.documents import Document
 class DocListItem(urwid.WidgetWrap):
     def __init__(self, doc):
         self.doc = doc
-        self.percent = doc.matchp
-        self.docid = self.doc.get_docid()
-        self.path = urwid.Text(self.doc.get_fullpaths()[0])
-        self.sources = self.doc.get_sources()
-        self.tags = urwid.Text(' '.join(self.doc.get_tags()))
-        self.url = urwid.Text(self.doc.get_url())
-        self.title = urwid.Text(self.doc.get_title())
-        self.authors = urwid.Text(self.doc.get_authors())
-        self.year = urwid.Text(self.doc.get_year())
-        self.data = urwid.Text(self.doc.get_data())
+        self.matchp = doc.matchp
+        self.docid = self.doc.docid
 
-        #self.tag_string = '(%s)' % (' '.join(self.tags))
-        self.source_string = '['
-        for source,sid in self.sources.items():
-            self.source_string += '%s:%s' % (source, sid)
-        self.source_string += ']'
+        #self.source_string = '[%s]' % ' '.join(self.doc.get_sources_list())
+        self.sources = urwid.Text(' '.join(self.doc.get_sources_list()))
+        self.tags = urwid.Text(' '.join(self.doc.get_tags()))
+        self.title = urwid.Text('')
+        self.authors = urwid.Text('')
+        self.year = urwid.Text('')
+        self.summary = urwid.Text('')
+
+        data = self.doc.get_bibdata()
+        if data:
+            if 'title' in data:
+                self.title = urwid.Text(data['title'])
+            if 'authors' in data:
+                astring = ' and '.join(data['authors'][:10])
+                if len(data['authors']) > 10:
+                    astring = astring + ' et. al.'
+                self.authors = urwid.Text(astring)
+            if 'year' in data:
+                self.year = urwid.Text(data['year'])
+
+        self.summary = urwid.Text(self.doc.get_data())
 
         self.c1width = 10
 
-        self.rowHeader = urwid.Columns(
-            [('fixed', self.c1width,
-              urwid.AttrWrap(
-                        urwid.Text('id:%s' % (self.docid)),
-                        'head_id',
-                        'focus_id')),
-             ('fixed', 5,
-              urwid.AttrWrap(
-                        urwid.Text('%i%%' % (self.percent)),
-                        'search_value_default',
-                        'focus')),
-             ('fixed', len(self.source_string),
-              urwid.AttrWrap(
-                        urwid.Text('%s' % (self.source_string)),
-                        'search_value_default',
-                        'focus')),
-             ],
-            )
+        self.rowHeader = urwid.AttrWrap(
+            urwid.Text('id:%s (%s)' % (self.docid, self.matchp)),
+            'head_id',
+            'focus_id')
+
+        # self.rowHeader = urwid.Columns(
+        #     [('fixed', self.c1width,
+        #       urwid.AttrWrap(
+        #                 urwid.Text('id:%s (%s)' % (self.docid, self.matchp)),
+        #                 'head_id',
+        #                 'focus_id')),
+        #      # ('fixed', 5,
+        #      #  urwid.AttrWrap(
+        #      #            urwid.Text('%i%%' % (self.percent)),
+        #      #            'search_value_default',
+        #      #            'focus')),
+        #      # ('fixed', len(self.source_string),
+        #      #  urwid.AttrWrap(
+        #      #            urwid.Text('%s' % (self.source_string)),
+        #      #            'search_value_default',
+        #      #            'focus')),
+        #      ],
+        #     )
 
         w = urwid.Pile(
             [
                 urwid.Divider('-'),
                 self.rowHeader,
+                self.docfield('sources', value_palette='search_sources'),
                 self.docfield('tags', value_palette='search_tags'),
                 self.docfield('title', value_palette='search_title'),
                 self.docfield('authors'),
                 self.docfield('year'),
-                self.docfield('path'),
-                self.docfield('url'),
-                self.docfield('data'),
+                self.docfield('summary'),
                 ]
             ,
             focus_item=1)
@@ -124,6 +136,9 @@ class Search(urwid.WidgetWrap):
     def viewEntry(self):
         docid = self.listbox.get_focus()[0].docid
         path = self.listbox.get_focus()[0].doc.get_fullpaths()[0]
+        if not path:
+            self.ui.set_status('ERROR: Could not find file for id:%s.' % docid)
+            return
         path = path.replace(' ','\ ')
         message = 'opening doc id:%s...' % docid
         self.ui.set_status(message)
@@ -133,9 +148,12 @@ class Search(urwid.WidgetWrap):
                         stderr=open('/dev/null','w'))
 
     def viewURL(self):
+        docid = self.listbox.get_focus()[0].docid
         url = self.listbox.get_focus()[0].doc.get_url()
-        message = 'opening url %s...' % url
-        self.ui.set_status(message)
+        if not url:
+            self.ui.set_status('ERROR: Could not determine url for id:%s.' % docid)
+            return
+        self.ui.set_status('opening url %s...' % url)
         subprocess.call(' '.join(["nohup", "jbrowser", url]) + ' &',
                         shell=True,
                         stdout=open('/dev/null','w'),
@@ -162,16 +180,18 @@ class Search(urwid.WidgetWrap):
         doc = db.doc_for_docid(docid)
         if sign is '+':
             msg = "Added tag '%s'" % (tag)
-            doc.add_tags(content)
+            doc.add_tags([tag])
         elif sign is '-':
             msg = "Removed tag '%s'" % (tag)
-            doc.remove_tags(tag)
+            doc.remove_tags([tag])
         doc.sync()
         tags = doc.get_tags()
         focus.tags.set_text(' '.join(tags))
         self.ui.set_status(msg)
 
     def setField(self, field):
+        self.ui.set_status('Not implemented')
+        return
         focus = self.listbox.get_focus()[0]
         element = eval('focus.' + field)
         value = element.get_text()[0]
