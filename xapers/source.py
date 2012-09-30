71e8bbd417b16880c9b3a79f2ab84f8211f015c4
Jameson Graef Rollins <jrollins@finestructure.net>
2012-09-30 12:51:00 -0700

    sources: enable all sources

--- xapers/source.py
+++ xapers/source.py
@@ -6,7 +6,6 @@ import xapers.sources
 
 def list_sources():
     sources = []
-    return ['doi', 'arxiv']
     for s in dir(xapers.sources):
         # skip the __init__ file when finding sources
         if '__' in s:
