026582ffd5a49a6f94485a7011a4687383a12507
Jameson Graef Rollins <jrollins@finestructure.net>
2012-10-02 11:47:41 -0700

    source/doi: use ADS for now, until entry key issue with dx.doi.org can be solved.
    dx.doi.org is returning bibtex entry keys with spaces in them for articles with long author lists.
    This breaks standard bibtex parsing.  Need to contact them about it, and hack a way to get around it.

--- xapers/sources/doi.py
+++ xapers/sources/doi.py
@@ -1,3 +1,4 @@
+import sys
 import urllib2
 
 class Source():
@@ -20,11 +21,22 @@ class Source():
         if 'file' in dir(self):
             f = open(self.file, 'r')
         else:
-            url = self.gen_url()
-            headers = dict(Accept='text/bibliography; style=bibtex')
-            req = urllib2.Request(url, headers=headers)
+            # FIXME: dx.doi.org returns bad bibtex keys for long author lists!
+            # url = self.gen_url()
+            # headers = dict(Accept='text/bibliography; style=bibtex')
+            # req = urllib2.Request(url, headers=headers)
+
+            req = 'http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=' + self.sid + '&data_type=BIBTEXPLUS'
+            #print >>sys.stderr, req
+
             f = urllib2.urlopen(req)
         bibtex = f.read()
         f.close
+
+        # need to take care of broken bibtex entry key from DOI for
+        # entries with 'et al' author lists
+        #import re
+        #re.compile('^.*{.*et al.*,')
+        #bibtex = bibtex.replace('et al.', 'et_al.', 1)
 
         return bibtex
