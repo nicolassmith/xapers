4fd704b35fd86b064021d3095228975c37128247
Jameson Graef Rollins <jrollins@finestructure.net>
2012-10-03 13:26:48 -0700

    sources/doi: clean up, and move back to dx.doi.org retrieval

--- xapers/sources/doi.py
+++ xapers/sources/doi.py
@@ -17,26 +17,38 @@ class Source():
         if loc.find(self.netloc) >= 0:
             self.sid = path.strip('/')
 
-    def get_bibtex(self):
-        if 'file' in dir(self):
-            f = open(self.file, 'r')
-        else:
-            # FIXME: dx.doi.org returns bad bibtex keys for long author lists!
-            # url = self.gen_url()
-            # headers = dict(Accept='text/bibliography; style=bibtex')
-            # req = urllib2.Request(url, headers=headers)
-
-            req = 'http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=' + self.sid + '&data_type=BIBTEXPLUS'
-            #print >>sys.stderr, req
-
-            f = urllib2.urlopen(req)
+    def _get_bib_file(self):
+        f = open(self.file, 'r')
         bibtex = f.read()
         f.close
+        return bibtex
 
+    def _get_bib_doi(self):
+        # FIXME: dx.doi.org returns bad bibtex keys for long author lists!
+        url = self.gen_url()
+        headers = dict(Accept='text/bibliography; style=bibtex')
+        req = urllib2.Request(url, headers=headers)
+        f = urllib2.urlopen(req)
+        bibtex = f.read()
+        f.close
         # need to take care of broken bibtex entry key from DOI for
         # entries with 'et al' author lists
         #import re
         #re.compile('^.*{.*et al.*,')
         #bibtex = bibtex.replace('et al.', 'et_al.', 1)
+        return bibtex
 
+    def _get_bib_ads(self):
+        req = 'http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=' + self.sid + '&data_type=BIBTEXPLUS'
+        f = urllib2.urlopen(req)
+        bibtex = f.read()
+        f.close
+        return bibtex
+
+    def get_bibtex(self):
+        if 'file' in dir(self):
+            bibtex = self._get_bib_file()
+        else:
+            bibtex = self._get_bib_doi()
+            # bibtex = self._get_bib_ads()
         return bibtex
