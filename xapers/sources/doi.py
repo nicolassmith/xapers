ccfb1a2c12e54afb086f051fad60c5696282bf71
Jameson Graef Rollins <jrollins@finestructure.net>
2012-10-04 10:32:13 -0700

    sources/doi: add new _get_bib_doi_json function
    This new function retrieves a json string DOI metadata.
    This json is better format than the bibtex, does not cause parsing failures,
    and includes full author list (not truncated).
    
    Should consider using this format for all sources, and just store document metadata as json string.

--- xapers/sources/doi.py
+++ xapers/sources/doi.py
@@ -1,6 +1,7 @@
 import io
 import sys
 import urllib2
+import xapers.bibtex as bibparse
 
 class Source():
     source = 'doi'
@@ -41,6 +42,7 @@ class Source():
         return bibtex
 
     def _get_bib_doi(self):
+        # http://www.crossref.org/CrossTech/2011/11/turning_dois_into_formatted_ci.html
         url = self.gen_url()
         headers = dict(Accept='text/bibliography; style=bibtex')
         req = urllib2.Request(url, headers=headers)
@@ -49,6 +51,18 @@ class Source():
         f.close
         # FIXME: this is a doi hack
         return self._clean_bibtex_key(bibtex)
+
+    def _get_bib_doi_json(self):
+        # http://www.crossref.org/CrossTech/2011/11/turning_dois_into_formatted_ci.html
+        url = self.gen_url()
+        headers = dict(Accept='application/citeproc+json')
+        req = urllib2.Request(url, headers=headers)
+        f = urllib2.urlopen(req)
+        json = f.read()
+        f.close
+        key = '%s:%s' % (self.source, self.sid)
+        bibentry = bibparse.json2bib(json, key)
+        return bibentry.as_string()
 
     def _get_bib_ads(self):
         req = 'http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=' + self.sid + '&data_type=BIBTEXPLUS'
@@ -61,6 +75,7 @@ class Source():
         if 'file' in dir(self):
             bibtex = self._get_bib_file()
         else:
-            bibtex = self._get_bib_doi()
             # bibtex = self._get_bib_ads()
+            # bibtex = self._get_bib_doi()
+            bibtex = self._get_bib_doi_json()
         return bibtex
