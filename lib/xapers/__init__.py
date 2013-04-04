5acb121b133b2daf827118db733457c7ed6c099f
Jameson Graef Rollins <jrollins@finestructure.net>
2013-04-04 10:57:36 -0700

    clean up __init__ imports

--- lib/xapers/__init__.py
+++ lib/xapers/__init__.py
@@ -17,8 +17,3 @@ along with notmuch.  If not, see <http://www.gnu.org/licenses/>.
 Copyright 2012
 Jameson Rollins <jrollins@finestructure.net>
 """
-
-from .database import Database
-from .documents import Documents, Document
-from .cli import UI
-from .nci import UI
