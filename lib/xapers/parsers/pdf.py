d7926e85f7536fd1cb1787b9970b0482dcc81956
Jameson Graef Rollins <jrollins@finestructure.net>
2013-04-14 14:30:27 -0700

    parser: redirect stderr to /dev/null
    Need to catch errors!
    
    also remove old comments.

--- lib/xapers/parsers/pdf.py
+++ lib/xapers/parsers/pdf.py
@@ -1,26 +1,9 @@
-# def _pdf2text(self, pdf):
-#     from subprocess import Popen, PIPE
-#     cmd = ['pdftotext', '-', '-']
-#     p = Popen(' '.join(cmd), stdin=PIPE, stdout=PIPE, shell=True)
-#     text = p.communicate(pdf.getvalue())[0]
-#     if p.wait() != 0:
-#         raise IOerror
-#     return text
-
 def parse_file(path):
-    # extract text from pdf
-    # import cStringIO
-    # pdf = cStringIO.StringIO()
-    # fi = open(os.path.join(self.xapers.root, path), 'rb')
-    # #fi = open(path, 'rb')
-    # pdf.write(fi.read())
-    # fi.close()
-    # text = self._pdf2text(pdf)
-
     from subprocess import Popen, check_output, PIPE, CalledProcessError
     cmd = ['pdftotext', path, '-']
+    #cmd = ['pdf2txt', path]
     # FIXME: figure out how to trap errors better
-    text = check_output(' '.join(cmd), shell=True)
+    text = check_output(' '.join(cmd), shell=True, stderr=open('/dev/null','w'))
     #p = Popen(' '.join(cmd), stdout=PIPE, shell=True)
     #text = p.communicate()[0]
     # FIXME: do something here?
