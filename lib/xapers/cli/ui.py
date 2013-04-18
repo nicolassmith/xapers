nicolassmith/xapers

commit fb8b9161c7741183c74906b0ad185ec084c20f00

cli: use codecs to set stdout writer to utf8
It's unclear if this is the correct thing to do, but it seems to fix
the problem with the output encoding differences between terminals and
pipes/files.  See e.g.:

http://stackoverflow.com/questions/492483/setting-the-correct-encoding-when-piping-stdout-in-python

lib/xapers/cli/ui.py
@@ -20,6 +20,8 @@ Jameson Rollins <jrollins@finestructure.net>
 
 import os
 import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 import sets
 import shutil
 import readline

commit 9bb329c26a573b66e2f4c50abc3e7dbbb0fee366

nci: hack to fix terminal encoding issue
Apparently fb8b9161c7741183c74906b0ad185ec084c20f00 introduced a
problem for the nci.  The above added the following:

sys.stdout = codecs.getwriter('utf8')(sys.stdout)

This apparently breaks the nci when presumably? utf-8 character
decoding is attempted:

UnicodeDecodeError: 'ascii' codec can't decode byte 0xc2 in position 63: ordinal not in range(128)

I most certainly understand what's going on here.  But storing the
original stdout method in the cli module, and then restoring it when
the nci is initiated seems to at least get around the problem.

lib/xapers/cli/ui.py
@@ -21,6 +21,7 @@ Jameson Rollins <jrollins@finestructure.net>
 import os
 import sys
 import codecs
+SYS_STDOUT = sys.stdout
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 import sets
 import shutil

lib/xapers/nci/ui.py
@@ -21,9 +21,16 @@ Jameson Rollins <jrollins@finestructure.net>
 import os
 import sys
 import urwid
+
 import subprocess
 
-from xapers.cli.ui import initdb
+# FIXME: what the hell?  Why do we have to do this?
+# it's because of
+# sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+# in cli.ui, but i don't understand why
+from xapers.cli.ui import initdb, SYS_STDOUT
+sys.stdout = SYS_STDOUT
+
 from search import Search
 from bibview import Bibview
 from help import Help